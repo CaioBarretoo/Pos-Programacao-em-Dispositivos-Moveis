090dcff4b0e0491f563b02d05232738690aad624
Caio Barreto <caio.barret@hotmail.com>
2024-07-07 10:50:23 -0300

    .

--- 1/Android Basico/FirebaseAuthApp/app/build.gradle.kts
+++ 1/Android Basico/FirebaseAuthApp/app/build.gradle.kts
@@ -54,5 +54,6 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("com.google.firebase:firebase-auth")
 }
